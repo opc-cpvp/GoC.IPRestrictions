cb4aba8577c1ff2e837c274c7d05619730160e09
Joshua Campbell <joshcampbell191@gmail.com>
2020-10-28 18:08:53 -0400

    initial commit

---
+++ src/GoC.IPRestrictions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoC.IPRestrictions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
